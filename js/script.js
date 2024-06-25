88177227b4516dda972a9bf43e574579cc448c60
MagicGionson93 <gpasini93@gmail.com>
2024-06-25 20:09:43 +0200

    aggiunte immagini e animazioni

--- js/script.js
+++ js/script.js
@@ -70,3 +70,31 @@ allLink.forEach(function (link) {
 const yearEl = document.querySelector(".year");
 const currentYear = new Date().getFullYear();
 yearEl.textContent = currentYear;
+
+
+
+const intersectionCallback = (entries) => {
+  for (const entry of entries) {
+    // Loop over all elements that either enter or exit the view.
+    if (entry.isIntersecting) {
+      // This is true when the element is in view.
+      entry.target.classList.add("show"); // Add a class.
+    }
+  }
+};
+
+/**
+ * Create a observer and use the instersectionCallback as
+ * the instructions for what to do when an element enters
+ * or leaves the view
+ */
+const observer2 = new IntersectionObserver(intersectionCallback);
+
+/**
+ * Get all .item elements and loop over them.
+ * Observe each individual item.
+ */
+const items = document.querySelectorAll(".step-text-box,.step-img-box");
+for (const item of items) {
+  observer2.observe(item);
+}
